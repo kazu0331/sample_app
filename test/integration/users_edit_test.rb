8a667aa5640a1154ab57c0a9eb83ac5fee62c43a
Kazuhiko Kamitani <kazukamitani106@gmail.com>
2018-05-27 19:01:32 +0900

    Finish user edit, update, index, and destroy actions

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  
+
+
+
+end
